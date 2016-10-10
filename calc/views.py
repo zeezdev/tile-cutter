b727ec7af121fb6a7260cf1d479d9cff9698cd29
Stanislav Tomilov <zeezdev@gmail.com>
2016-10-11 03:39:18 +0500

    Init

---
+++ calc/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+from .forms import CalcFloorForm, LAYING_METHOD_DIRECT
+
+
+def floor(request):
+    form = CalcFloorForm(initial={
+        'method': LAYING_METHOD_DIRECT,
+        'width': 5.0, 'length': 5.0,
+        'tile_width': 400,
+        'tile_length': 400,
+        'delimiter': 1.5
+    })
+
+    result = None
+    cost = None
+    image = None
+
+    if request.method == 'POST':
+        form = CalcFloorForm(request.POST)
+        if form.is_valid():
+            result, cost, image = form.calc()
+        # else:
+        #     result = "Ошибка"
+
+    return render(request, 'calc-floor.html', {'form': form, 'result': result, 'cost': cost, 'draw': image})
