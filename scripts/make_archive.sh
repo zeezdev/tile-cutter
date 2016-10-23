1a071757782f43094357e7e36d2f30915265feee
Stanislav Tomilov <zeezdev@gmail.com>
2016-10-24 01:45:15 +0500

    add make_archive.sh for deploy

---
+++ scripts/make_archive.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -x
+
+SCRIPT=$(readlink -f "$0")
+# SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+SCRIPTPATH=`dirname $SCRIPT`
+DIR=$(dirname "$SCRIPTPATH")
+
+ARCHNAME="cutter.tar.gz"
+HOSTNAME="tc"
+
+echo ${DIR}
+
+rm ${DIR}/${ARCHNAME}
+
+tar -zcvf ${DIR}/${ARCHNAME} \
+--exclude="./.idea" \
+--exclude="./.venv" \
+--exclude="./.git" \
+--exclude="./${ARCHNAME}" \
+--exclude="./ideas.txt" \
+--exclude="./passwd.txt" \
+--exclude="./.gitignore" \
+--exclude="*.pyc" \
+--exclude="./*/__pycache__" \
+-C ${DIR} .
+
+scp ${DIR}/${ARCHNAME} ${HOSTNAME}:/root/tmp/
