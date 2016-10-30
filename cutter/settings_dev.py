c9d096bd831e19258b210391da946d7dbb5b132f
Stanislav Tomilov <zeezdev@gmail.com>
2016-10-31 02:20:39 +0500

    Web: fix footer; Settings: add requrements folder;

--- cutter/settings_dev.py
+++ cutter/settings_dev.py
@@ -117,7 +117,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
