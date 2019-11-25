d943de6e49f4d730d91684e6c8c6c081843afed7
Stanislav Tomilov <zeezdev@gmail.com>
2019-11-26 01:47:13 +0500

    Fix deploy requirements and production logging

--- cutter/settings/production.py
+++ cutter/settings/production.py
@@ -57,6 +57,26 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
